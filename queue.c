cf2c06615455373d7b384b5c2698afaa25e4ac9f
Omer Drori <73065220+Omerd05@users.noreply.github.com>
2024-07-20 17:03:36 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -17,49 +17,46 @@ struct lnkList {
     size_t sz;
 };
 
-void add(struct lnkList* lst, struct Node*) {
-
+void add(struct lnkList* lst, struct Node* vertex) {
+    if(lst->head == NULL) {
+        lst->tail = lst->head = vertex;
+    }
+    else {
+        lst->tail->nxt = vertex;
+        lst->tail = lst->tail->nxt;
+    }
 }
 void* pop(struct lnkList* lst) {
-
+    void* result = lst->head->item;
+    struct Node* tmp = lst->head;
+    lst->head = tmp->nxt;
+    free(tmp);
+    if(lst->head == NULL) {
+        lst->tail = NULL;
+    }
+    return result;
 }
 
 struct lnkList* tasks;
 struct lnkList* cvList;
-atomic_int dqConut; //Waiting for tasks, not for lock!
+atomic_int dqConut; //number of threads waiting for tasks, not for lock!
+atomic_int done;
 
 
 mtx_t queueLock;
 
-/*void* getItem() { //returns with matching enqueued item
-    struct Node* curr = (struct Node*)calloc(sizeof(struct Node),1);
-    curr->id = thrd_current();
-    curr->nxt = NULL;
-    struct Node* prev = thrSlep->tail;
-    add(thrSlep,curr);
-    thrd_join(prev->id,NULL);
-    void* result = NULL;
-    if(tasks->sz == 0) {
-        cnd_wait(&idle,NULL);
-    }
-    return pop(tasks);
-}*/
-
 void initQueue() {
     mtx_init(&queueLock, mtx_plain);
     cvList = (struct lnkList*)calloc(sizeof(struct lnkList),1);
-    (struct Node*)calloc(sizeof(struct Node),1);
-
-    thrd_exit(EXIT_SUCCESS);
-
-    /*qData = (struct lnkList*)calloc(sizeof(struct lnkList),1);
-    qData->sentinel = (struct Node*)calloc(sizeof(struct Node),1);
-    thrSupps = (struct lnkList*)calloc(sizeof(struct lnkList),1);
-    thrSupps->sentinel = (struct Node*)calloc(sizeof(struct Node),1);
-
-    mtx_init(&inputLock,mtx_plain);
-    mtx_init(&suppLock,mtx_plain);*/
-    //thrd_exit(EXIT_SUCCESS);
+    cvList->sentinel = (struct Node*)calloc(sizeof(struct Node),1);
+    //cvList->head = cvList->sentinel;
+    //cvList->tail = cvList->sentinel;
+    tasks = (struct lnkList*)calloc(sizeof(struct lnkList),1);
+    tasks->sentinel = (struct Node*)calloc(sizeof(struct Node),1);
+    //tasks->head = tasks->sentinel;
+    //tasks->tail = tasks->sentinel;
+    dqConut = 0;
+    done = 0;
 }
 
 void enqueue(void* data) {
@@ -77,7 +74,6 @@ void enqueue(void* data) {
 
 void* dequeue(void) {
     mtx_lock(&queueLock);
-
     if(tasks->sz - dqConut <= 0) { //aka we wait for new task.
         cnd_t cv;
         cnd_init(&cv);
@@ -99,8 +95,43 @@ void* dequeue(void) {
         cnd_signal(&curr->nxt->cv);
     }
     mtx_unlock(&queueLock);
+    done++;
     return result;
     //WARNING WARNING WARNING - now that we've unlocked another thread may print before us even if we were first.*/
+}
+
+bool tryDequeue(void ** val) {
+    if(mtx_trylock(&queueLock) != thrd_success) {
+        return false;
+    }
+    if(tasks->sz - dqConut <= 0) {
+        mtx_unlock(&queueLock);
+        return false;
+    }
+    if(tasks->sz - dqConut <= 0) { //aka we wait for new task.
+        cnd_t cv;
+        cnd_init(&cv);
+        struct Node* element = (struct Node*)calloc(sizeof(struct Node),1);
+        element->cv = cv;
+        element->nxt = NULL;
+        add(cvList,element);
+        //cvList->sz++;
+    }
+    dqConut++;
+    struct Node* curr = cvList->tail;
+    if(dqConut > 1 || tasks->sz == 0) {
+        cnd_wait(&cvList->tail->cv,&queueLock);
+    }
+    void* result = pop(tasks);
+    tasks->sz--;
+    dqConut--;
+    if(tasks->sz > 0 && dqConut > 0) { //aka somebody is sleeping and waiting for us
+        cnd_signal(&curr->nxt->cv);
+    }
+    mtx_unlock(&queueLock);
+    done++;
+    *val = result;
+    return true;
 }
 
 void destroyQueue() {
@@ -114,9 +145,20 @@ void destroyQueue() {
     last = cvList->sentinel;
     curr = last->nxt;
     while(last != NULL) {
+        cnd_destroy(&last->cv);
         free(last);
         last = curr;
     }
     free(cvList);
+    mtx_destroy(&queueLock);
 }
 
+size_t size(void) {
+    return tasks->sz;
+}
+size_t waiting(void) {
+    return dqConut;
+}
+size_t visited(void) {
+    return done;
+}
