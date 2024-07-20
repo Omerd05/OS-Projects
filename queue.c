d6e1ea7d7afa4c4c2bf7067dc8237595f04c475a
Omer Drori <73065220+Omerd05@users.noreply.github.com>
2024-07-20 13:49:49 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,10 +1,12 @@
 #include <threads.h>
 #include "queue.h"
 #include <stdlib.h>
+#include <stdatomic.h>
 
 struct Node {
     void* item;
     thrd_t id;
+    cnd_t cv;
     struct Node* nxt;
 };
 
@@ -12,93 +14,109 @@ struct lnkList {
     struct Node* sentinel;
     struct Node* head;
     struct Node* tail;
-    size_t sz = 0;
+    size_t sz;
 };
 
 void add(struct lnkList* lst, struct Node*) {
 
 }
-void pop(struct lnkList* lst) {
+void* pop(struct lnkList* lst) {
 
 }
 
+struct lnkList* tasks;
+struct lnkList* cvList;
+atomic_int dqConut; //Waiting for tasks, not for lock!
 
-struct lnkList* qData;
-struct lnkList* thrSupps;
 
+mtx_t queueLock;
 
-mtx_t inputLock;
-mtx_t suppLock;
-
-cnd_t qInput;
-
-void supporter() {
-    struct Node* element = (struct Node*)calloc(sizeof(struct Node),1);
-    element->id = thrd_current();
-    element->nxt = NULL;
-    add(thrSupps,element);
-}
+/*void* getItem() { //returns with matching enqueued item
+    struct Node* curr = (struct Node*)calloc(sizeof(struct Node),1);
+    curr->id = thrd_current();
+    curr->nxt = NULL;
+    struct Node* prev = thrSlep->tail;
+    add(thrSlep,curr);
+    thrd_join(prev->id,NULL);
+    void* result = NULL;
+    if(tasks->sz == 0) {
+        cnd_wait(&idle,NULL);
+    }
+    return pop(tasks);
+}*/
 
 void initQueue() {
-    qData = (struct lnkList*)calloc(sizeof(struct lnkList),1);
+    mtx_init(&queueLock, mtx_plain);
+    cvList = (struct lnkList*)calloc(sizeof(struct lnkList),1);
+    (struct Node*)calloc(sizeof(struct Node),1);
+
+    thrd_exit(EXIT_SUCCESS);
+
+    /*qData = (struct lnkList*)calloc(sizeof(struct lnkList),1);
     qData->sentinel = (struct Node*)calloc(sizeof(struct Node),1);
     thrSupps = (struct lnkList*)calloc(sizeof(struct lnkList),1);
     thrSupps->sentinel = (struct Node*)calloc(sizeof(struct Node),1);
 
     mtx_init(&inputLock,mtx_plain);
-    mtx_init(&suppLock,mtx_plain);
+    mtx_init(&suppLock,mtx_plain);*/
     //thrd_exit(EXIT_SUCCESS);
 }
 
 void enqueue(void* data) {
-    mtx_lock(&inputLock);
-
-    cnd_signal(&qInput);
-    mtx_unlock(&inputLock);
+    mtx_lock(&queueLock);
+    struct Node* element = (struct Node*)calloc(sizeof(struct Node),1);
+    element->item = data;
+    element->nxt = NULL;
+    add(tasks,element);
+    tasks->sz++;
+    if(dqConut > 0) {
+        cnd_signal(&cvList->head->cv);
+    }
+    mtx_unlock(&queueLock);
 }
 
 void* dequeue(void) {
+    mtx_lock(&queueLock);
 
-    //mtx_lock(&thrLock);
-    if(thrSupps->sz - qData->sz > 0) {
-
-        thrd_t agent;
-        mtx_lock(&suppLock);
-        thrd_create(agent,supporter,NULL);
-        mtx_lock(&suppLock);
-        thrd_join(agent,NULL);
-
-        /*struct Node* element = (struct Node*)calloc(sizeof(struct Node),1);
-        element->id = thrd_current();
+    if(tasks->sz - dqConut <= 0) { //aka we wait for new task.
+        cnd_t cv;
+        cnd_init(&cv);
+        struct Node* element = (struct Node*)calloc(sizeof(struct Node),1);
+        element->cv = cv;
         element->nxt = NULL;
-        add(thrSleep,element);
-        if(thrSleep>0) {
-            thrd_join(thrSleep->head->id,NULL);
-        }
-        else {
-            cnd_wait(&qInput, &thrLock);
-        }*/
+        add(cvList,element);
+        //cvList->sz++;
     }
-
-    void* result = qData->head->item;
-    pop(qData);
-    //mtx_unlock(&thrLock);
+    dqConut++;
+    struct Node* curr = cvList->tail;
+    if(dqConut > 1 || tasks->sz == 0) {
+        cnd_wait(&cvList->tail->cv,&queueLock);
+    }
+    void* result = pop(tasks);
+    tasks->sz--;
+    dqConut--;
+    if(tasks->sz > 0 && dqConut > 0) { //aka somebody is sleeping and waiting for us
+        cnd_signal(&curr->nxt->cv);
+    }
+    mtx_unlock(&queueLock);
     return result;
+    //WARNING WARNING WARNING - now that we've unlocked another thread may print before us even if we were first.*/
 }
 
 void destroyQueue() {
-    struct Node* last = vals->sentinel;
+    struct Node* last = tasks->sentinel;
     struct Node* curr = last->nxt;
     while(last != NULL) {
         free(last);
         last = curr;
     }
-    free(vals);
-    last = thrs->sentinel;
+    free(tasks);
+    last = cvList->sentinel;
     curr = last->nxt;
     while(last != NULL) {
         free(last);
         last = curr;
     }
-    free(thrs);
+    free(cvList);
 }
+
